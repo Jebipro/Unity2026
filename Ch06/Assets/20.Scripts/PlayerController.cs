3f479a28d84debd291b133cbef4b71c56cae58f2
Jebipro <ghehfdl923@gmail.com>
2026-04-15 15:28:36 +0900

    Ch06

---
+++ Ch06/Assets/20.Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float jumpForce = 400f;
+    float walkForce = 30f;
+    float maxWalkSpeed = 2f;
+
+    public Sprite[] walkSprites;
+    float time = 0;
+    int idx = 0;
+
+
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+
+        if(rb.linearVelocityX < maxWalkSpeed)
+        {
+            rb.AddForce(transform.right * walkForce);
+        }
+    }
+}
