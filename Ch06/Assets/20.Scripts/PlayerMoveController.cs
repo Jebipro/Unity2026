090b7ac67d2660f92fa9773dd8bc10947d21d61b
Jebipro <ghehfdl923@gmail.com>
2026-05-04 09:37:30 +0900

    Ch06

---
+++ Ch06/Assets/20.Scripts/PlayerMoveController.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    float jumpForce = 380f;
+    public float walkForce = 15f;
+    float maxWalkSpeed = 1f;
+    Animator anim;
+
+    public Sprite[] walkSprites;
+    public Sprite jumpSprite;
+    public float animationPeriod = 0.2f;
+    float time = 0;
+    int idx = 0;
+    int key = 0;
+    SpriteRenderer sr;
+
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        key = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            key = 1;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            key = -1;
+        }
+
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        if (Input.GetMouseButtonDown(0) &&
+                rb.linearVelocityY == 0)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+
+        if (Mathf.Abs(rb.linearVelocityX) < maxWalkSpeed)
+        {
+            rb.AddForce(transform.right * walkForce * key);
+        }
+
+        time += Time.deltaTime;
+
+        if (rb.linearVelocityY != 0)
+        {
+            anim.SetBool("IsJumping", true);
+        }
+        else //if (time > animationPeriod)
+        {
+            anim.SetBool("IsJumping", false);
+        }
+
+
+        //if (rb.linearVelocityY != 0)
+        //{
+        //    sr.sprite = jumpSprite;
+        //}
+        //else if (time > animationPeriod)
+        //{
+        //    time = 0;
+        //    sr.sprite = walkSprites[idx];
+        //    idx++;
+        //    if (idx == walkSprites.Length)
+        //    {
+        //        idx = 0;
+        //    }
+        //}
+
+        anim.speed = Mathf.Abs(rb.linearVelocityX);
+
+        if (transform.position.y < -8)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("ClearScene");
+        Debug.Log("¼º°ø");
+    }
+}
