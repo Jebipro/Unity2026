4a2f1a4af26f1fdd78acde706fb1fcf7b632fcf2
Jebipro <ghehfdl923@gmail.com>
2026-05-20 14:50:19 +0900

    Ch08

--- Ch08/Assets/20.Scripts/ItemGenerator.cs
+++ Ch08/Assets/20.Scripts/ItemGenerator.cs
@@ -5,16 +5,30 @@ public class ItemGenerator : MonoBehaviour
     public GameObject applePrefab;
     public GameObject bombPrefab;
 
-    public float span = 1.0f;
+    public float span = 1.5f;
+    public int ratio = 3;
+
     float delta = 0;
 
     // Update is called once per frame
     void Update()
     {
         delta += Time.deltaTime;
+        GameObject item;
         if (delta > span)
         {
-            GameObject item = Instantiate(applePrefab);
+            int dice = Random.Range(0, 10);
+            if (dice < ratio)
+            {
+                item = Instantiate(bombPrefab);
+            }
+            else
+            {
+                item = Instantiate(applePrefab);
+            }
+
+
+            //GameObject item = Instantiate(applePrefab);
             float x = Random.Range(-1, 2);
             float z = Random.Range(-1, 2);
             item.transform.SetParent(transform);
